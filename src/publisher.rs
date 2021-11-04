2b0d2dc96522f95c3affd0aa6e74bd9ecdb0dd6c
Sitt Min Oo <sittminoo1995145@gmail.com>
2021-11-04 13:50:29 +0100

    update documentation

--- src/publisher.rs
+++ src/publisher.rs
@@ -1,18 +1,23 @@
+//!
+//! This module is responsible for handling the incoming connections and 
+//! stream data through the websockets. 
+//!
+//! The sub-modules containing the concrete implementation of the 
+//! [`Publisher`] trait will be used to publish the data with varying data stream characteristics. 
+//!
+//!
+//! [`Publisher`]: Publisher 
 use crate::processor::{Processor, Record};
-
 use crate::util::{self, StreamConfig};
 use async_trait::async_trait;
 use futures_util::stream::SplitSink;
 use futures_util::StreamExt;
 use log::{debug, error, info};
 use std::collections::HashMap;
-
 use std::sync::Arc;
-
 use std::io::Error as StdError;
 use std::io::ErrorKind as StdErrorKind;
 use std::net::SocketAddr;
-
 use tokio::io::AsyncBufReadExt;
 use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::RwLock;
@@ -20,16 +25,17 @@ use tokio_tungstenite::tungstenite::{Message, Result};
 use tokio_tungstenite::{accept_async, tungstenite::Error};
 pub mod constant;
 
-///shorthand type associations
 type SharedData<F> = Arc<RwLock<GroupedData<F>>>;
 type DataKey<P> = <<P as Processor>::Model as Record>::Key;
 type Data<P> = <P as Processor>::Model;
 type GroupedData<F> = HashMap<DataKey<F>, Vec<Data<F>>>;
 type Proc<Pub> = <Pub as Publisher>::Processor;
 
-///
+
+
 /// Starts a TCP stream for the given stream configuration.
-/// New web socekts will be spawned in tokio async threads for each new connection.
+///
+/// Each new web socekts will be handled in their own tokio async threads.
 pub async fn start_stream<Pub: 'static>(
     config_struct: StreamConfig,
     publisher: &'static Pub,
@@ -40,18 +46,16 @@ where
     let addr = format!("{}:{}", config_struct.ip, config_struct.port);
     let data_root = config_struct.data_folder.unwrap();
     info!(
-        "For connection: {}:{}\nReading files from the given data root directory: {}",
-        config_struct.ip, config_struct.port, data_root
-    );
+        "For connection: {}:{}, reading files from the given data root directory: {}",
+        config_struct.ip, config_struct.port, data_root);
 
     let data = read_file_in_mem::<Proc<Pub>>(data_root).await?;
+    info!("Finished reading and grouping data: {}", data_root);
 
-    info!("Finished reading and grouping");
 
     let shared_data = Arc::new(RwLock::new(data));
-
     let listener = TcpListener::bind(&addr).await.expect("Can't listen");
-    info!("Listening on: {}", addr);
+    info!("Listening on: {}, with publishing mode: {:?}", addr, config_struct.mode);
 
     while let Ok((stream, _)) = listener.accept().await {
         let peer = stream
@@ -60,12 +64,22 @@ where
 
         let shared_data = shared_data.clone();
         info!("Peer address: {}", peer);
-
         tokio::spawn(accept_connection(peer, stream, shared_data, publisher));
     }
+
     Ok(())
 }
 
+
+/// Returns the data [records] in a [`HashMap`] grouped by a [key]. 
+///
+/// Reads all the files located under the given data root folder and process 
+/// them with a data [`Processor`]. The processor will group the [records] by 
+/// their corresponding [key]. 
+///
+/// [records]: crate::processor::Record
+/// [key]: crate::processor::Record::Key
+/// [`Processor`]: crate::processor::Processor
 async fn read_file_in_mem<F: 'static>(data_root: &str) -> Result<HashMap<DataKey<F>, Vec<Data<F>>>>
 where
     F: Processor,
@@ -97,6 +111,10 @@ where
     Ok(data)
 }
 
+
+
+/// Accepts the incoming client connection and handle it for 
+/// data streaming. 
 async fn accept_connection<Pub: 'static>(
     peer: std::net::SocketAddr,
     stream: tokio::net::TcpStream,
@@ -113,6 +131,12 @@ async fn accept_connection<Pub: 'static>(
     }
 }
 
+/// Handle the incoming connection by opening a websocket.
+///
+/// The peer will start receiving streaming data with the streaming behaviour 
+/// as defined by the [`Publisher`]'s implementations.
+/// 
+/// [`Publisher`]: crate::publisher::Publisher 
 async fn handle_connection<Pub>(
     peer: SocketAddr,
     stream: TcpStream,
@@ -129,6 +153,13 @@ where
     Ok(())
 }
 
+/// Publishes data through a websocket connection.
+///
+/// The behaviour of the data stream (i.e. stream-rate, delays) will be defined 
+/// by the underlying implementations of this trait. The given [`Processor`] is 
+/// used to define the type of data being published.
+///
+/// [`Processor`]: Publisher::Processor
 #[async_trait]
 pub trait Publisher {
     type Processor: Processor;
